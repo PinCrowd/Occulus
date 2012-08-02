c626867d8060d35b240c3fce528320d1cbe4ff08
Robert Allen <zircote@gmail.com>
2012-08-01 19:30:35 -0500

    updated and added new files and vectors

--- pin_detection.py
+++ pin_detection.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python
 import os
-import numpy as np
 import cv2
 import cv2.cv as cv
 
+"1343772283-HAAR-ALL-GAB-0.5-10000_5000"
+"1343751055-HAAR-GAB-0.45-20000_10000"
 
-cascade_fn = "lbpcascades/lbpcascade_frontalface.xml"
-in_image_fn  = ""
-out_image_fn = ""
+cascade_fn = "cascades/1343845877-HAAR-ALL-GAB-0.5-3000_1500/cascade.xml"
+in_image_fn  = "images/positives/pins-cropped.png"
+out_image_fn = "images/detected.jpg"
 cascade = cv2.CascadeClassifier(cascade_fn)
+
 if os.path.exists(in_image_fn) & os.path.exists(cascade_fn):
     img = cv2.imread(in_image_fn)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    gray = cv2.equalizeHist(gray)
+#    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#    gray = cv2.equalizeHist(gray)
     rectangles = cascade.detectMultiScale(img, scaleFactor=1.001, minNeighbors=1,
-        flags=0,
-        minSize=(80, 80), maxSize=(160, 160))
+        flags=0|cv.CV_HAAR_SCALE_IMAGE,
+        minSize=(60, 60), maxSize=(160, 160))
+
+    print(rectangles)
+    print(len(rectangles))
+
     for x, y, width, height in rectangles:
         cv2.rectangle(img, (x, y), (x + width, y + height), (0, 0, 255), 2)
     cv2.imwrite(out_image_fn, img)
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Occulus',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='zircote',
+    author_email='',
+    description=''
+)
